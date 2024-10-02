e326d487a7ce18bf6bdda7f8f3d3e8a39480eefd
Rynnnnnnnn <163542461+Rynnnnnnnn@users.noreply.github.com>
2024-10-02 15:53:58 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require_once 'core/dbConfig.php'; ?>
+
+<!DOCTYPE html>
+<html lanng="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="witdth=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+</body>
+</html>
